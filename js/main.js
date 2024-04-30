b9788a5e9e91f7f40da56649ebc2a3c83f4e11fb
MimoSYR <66282994+MimoSYR@users.noreply.github.com>
2024-04-29 22:54:19 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// owl  carousel
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items : 3,
+        autoplay : true,
+        loop : true,
+        autoplayTimeout : 2000
+    });
+  });
+
+  
